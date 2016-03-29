972b9bb141d9b8c702f7547475479a5d3ab28da2
jw6644 <jeremy_wong67@hotmail.com>
2016-03-28 19:45:30 -0700

    V3
    Got loading page working, Added loading gif, Unified typography better,
    Got cookies working

---
+++ V3 Mar. 28, 2016/js/main.js
@@ -0,0 +1,27 @@
+$('#nav-icon3,#nav-icon3grey').click(
+  function(){
+    $('.navmenu').toggleClass('hide')
+  }
+)
+
+
+$(document).ready(function(){
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4,#nav-icon3grey').click(function(){
+        $(this).toggleClass('open');
+    });
+});
+
+$('.enter').click(function(){
+   $('.loadingpage').addClass('byebye');
+});
+
+$('.bigimage').scroll(function(){
+    $("div").removeClass('hide');
+    $(".bigimage").addClass('hide');
+  });
+
+$('.casestudy').click(function(){
+    $('#projectpage div').removeClass('hide');
+    $(".bigimage").addClass('hide');
+  }
+)
